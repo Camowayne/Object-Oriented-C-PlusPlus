9def0c2e3e8625e074874705b9228bc3b6642ec3
Camowayne <vcamowayne@gmail.com>
2024-02-01 14:02:46 -0800

    First draft

---
+++ Lab_!/Hello.cpp
@@ -0,0 +1,13 @@
+/***
+ * This lab is an introduction to C++ and aims to teach me the basics of the coding language
+***/
+
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main() {
+    cout <<"Hello World!" << endl;
+    printf("Hello World again!\n");
+    return 0;
+}
