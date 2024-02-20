88b639e7489f96cb2f4a36314e68b49d757686e3
Camowayne <vcamowayne@gmail.com>
2024-02-20 13:57:16 -0800

    Wrote code for main()
    Wrote main() portion of code along with code to call aforementioned functions

--- CSE 2010 C++/Lab_4/Lab4_func.cpp
+++ CSE 2010 C++/Lab_4/Lab4_func.cpp
@@ -54,4 +54,27 @@ template <typename T>
 void myPrint(string func_type, T output){
     cout << select(func_type) << output << endl;
 }
+int main(){
+    // initialize 3 int parameters
+    int x1 = 2;
+    int x2 = 3;
+    int x3 = 5;
 
+    // call sumOfThree
+    int sum_out = sumOfThree(x1, x2, x3);
+    // call myPrint
+    myPrint("sum", sum_out);
+
+    //initialize 2 double parameters
+    double dividend = 12345.1;
+    double divisor = 2345.6;
+
+    // call division
+    double div_out = division(dividend, divisor);
+    myPrint("division", div_out);
+
+    // compare div_out and sum_out
+    bool is_greater_out = isGreaterThan(div_out, sum_out);
+    myPrint("comparison", is_greater_out);
+    return 0;
+}
