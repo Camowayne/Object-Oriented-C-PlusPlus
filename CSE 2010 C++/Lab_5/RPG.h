ba34944f8c18367ee92d54f6de9156ee1128c833
Camowayne <vcamowayne@gmail.com>
2024-02-29 13:19:12 -0800

    Update RPG.h

---
+++ CSE 2010 C++/Lab_5/RPG.h
@@ -0,0 +1,19 @@
+#include <iostream>
+#ifndef RPG_H
+#define RPG_H
+
+using namespace std;
+
+const int SKILL_SIZE = 2;
+
+class RPG {
+    public:
+        //constructors
+        RPG();
+        RPG(string name, int health, int strength, int defense, string type);
+    private:
+        string type; // warrior, mage, thief, archer
+        string skills[SKILL_SIZE];
+
+};
+#endif
